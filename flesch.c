689bc2feb3f2de2da972d80888a6158e95366c5f
enkyuan <yuan_enkang@outlook.com>
2026-03-13 15:30:53 -0400

    fix: infinite loop + counting sentences

--- flesch.c
+++ flesch.c
@@ -10,68 +10,72 @@
 #include "flesch.h"
 
 int countSentences(const char* text) {
-    // TODO: Implement this function
-	int sentences = 0;
-	const char* ptr = text;
+    int sentences = 0;
+    const char* ptr = text;
 
-	while (*ptr) {
-		if (*ptr == '.' || *ptr == ':' || *ptr == ';' || 
+    while (*ptr) {
+        if (*ptr == '.' || *ptr == ':' || *ptr == ';' ||
             *ptr == '?' || *ptr == '!') {
             sentences++;
         }
+        ptr++;
+    }
 
-		ptr++;
-	}
+    if (sentences == 0 && *text != '\0') {
+        return 1;
+    }
 
     return sentences;
 }
 
 int countWords(const char* text) {
-    // TODO: Implement this function
-	int words = 0;
-	bool inWord = false;
-	const char* ptr = text;
-	
-	while (*ptr) {
-		bool isDelimiter = isspace(*ptr) || (*ptr == '.' || *ptr == ':' || *ptr == ';' || *ptr == '?' || *ptr == '!');
+    int words = 0;
+    bool inWord = false;
+    const char* ptr = text;
 
-		if (!isDelimiter && !inWord) {
-			inWord = true;
-			words++;
-		} else if (isDelimiter) {
-			inWord = false;
-		}
+    while (*ptr) {
+        bool isDelimiter = isspace((unsigned char)*ptr) ||
+                           *ptr == '.' || *ptr == ':' || *ptr == ';' ||
+                           *ptr == '?' || *ptr == '!';
 
-		ptr++;
-	}
+        if (!isDelimiter && !inWord) {
+            inWord = true;
+            words++;
+        } else if (isDelimiter) {
+            inWord = false;
+        }
+
+        ptr++;
+    }
 
     return words;
 }
 
 int countSyllables(const char* text) {
-    // TODO: Implement this function
-	int syllables = 0;
-	bool inVowelGroup = false;
-	const char *ptr = text;  	
-	
-	while (*ptr) {
-		char c = tolower(*ptr);
-		bool isVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-		
-		if (isVowel && !inVowelGroup) {
-			inVowelGroup = true;
-			syllables++;	
-		} else if (!isVowel) {
-			inVowelGroup = false;
-		}
-	}
+    int syllables = 0;
+    bool inVowelGroup = false;
+    const char* ptr = text;
 
-	return syllables;
+    while (*ptr) {
+        char c = tolower((unsigned char)*ptr);
+        bool isVowel = (c == 'a' || c == 'e' || c == 'i' ||
+                        c == 'o' || c == 'u');
+
+        if (isVowel && !inVowelGroup) {
+            inVowelGroup = true;
+            syllables++;
+        } else if (!isVowel) {
+            inVowelGroup = false;
+        }
+
+        ptr++;
+    }
+
+    return syllables;
 }
 
 double calculateFleschScore(TextStats stats) {
-    // TODO: Implement this function
-	if (stats.words == 0) {
+    if (stats.words == 0) {
         return 0.0;
     }
 
@@ -79,9 +83,7 @@ double calculateFleschScore(TextStats stats) {
     double sentences = (double)stats.sentences;
     double syllables = (double)stats.syllables;
 
-    double score = 206.835 - (1.015 * (words / sentences)) - (84.6 * (syllables / words));
-
-    return score;
+    return 206.835 - (1.015 * (words / sentences)) - (84.6 * (syllables / words));
 }
 
 TextStats analyzeText(const char* text) {
